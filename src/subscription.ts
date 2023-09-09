8703d32d2bc229f9ba473fc0c1692fe8a272a770
srav001 <sravansuresh1x@gmail.com>
2023-09-09 23:32:42 +0530

    feat: type changes

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,6 +1,56 @@
 import { dynamicallyExecuteFunction } from './functions/helpers';
 import { createSignal} from 'solid-js';
 
+type ValueMutator<SubType> = (val: SubType) => SubType;
+type Subscriber<SubType> = (val: SubType) => Promise<void> | void;
+
+type Signal<T> = {
+    value: T & ({} | null);
+    get(): T & ({} | null);
+    set(val: (T & ({} | null)) | ValueMutator<T>): void;
+    	/**
+		 * A Subscriber(function) is executed when the value is changed.
+		 * @param subscriber - type Subscriber = (val: T) => Promise<void> | void;
+		 */
+
+    addSub(subscriber: Subscriber<T>): void;
+    deleteSub(subscriber: Subscriber<T>): void;
+    triggerSubs(): void;
+}
+
+type SignalWithMutate<T> = Signal<T> & {
+	/**
+		 * It mutates the value of the object.
+		 * @param mutator - (val: T) => T
+		 */
+	mutate(mutator: ValueMutator<T>): void
+}
+	/**
+	 * Takes a function as an argument and adds it to the subscriber list.
+	 * @param subscriber - Subscriber
+	 */
+	function addSubscriber<T>(subscriber: Subscriber<T>, subscriptions: Set<Subscriber<T>> ) {
+		if (typeof subscriber !== 'function') {
+			throw new Error('Subscriber must be a function');
+		}
+		subscriptions.add(subscriber);
+	}
+	/**
+	 * It deletes an event/subscriber from the subscriptions list.
+	 * @param subscriber - Subscriber
+	 */
+	function deleteSubscriber<T>(subscriber: Subscriber<T>,subscriptions: Set<Subscriber<T>> ) {
+		subscriptions.delete(subscriber);
+	}
+	/**
+	 * It loops through the Set of subscribers and executes each function with the value passed in as
+	 * an argument
+	 * @param {SubType} val - The value that is being passed to the subscribers.
+	 */
+	function triggerSubscribers<T>(val: T, subscriptions: Set<Subscriber<T>> ) {
+		subscriptions.forEach(dep => dynamicallyExecuteFunction(dep, val));
+}
+	
 /**
  * It takes a value and returns an object with a value property that is a shallowRef of the value.
  * passed in, and Subscribers(function) are added to a list to be executed when the value is changed.
@@ -8,7 +58,7 @@ import { createSignal} from 'solid-js';
  * @returns A function that returns an object with a shallow reactive value, a subscriber and a
  * few extra methods.
  */
-export function useSubscription<T>(value: T, deep = false) {
+export function useSignal<T, P extends boolean | undefined = undefined>(value: T, primitive?: P ) {
 	if (value === undefined) {
 		throw new Error('No value provided, Initial value is required');
 	}
@@ -16,53 +66,26 @@ export function useSubscription<T>(value: T, deep = false) {
 	const [subGetter, subSetter] = createSignal(value);
 	type SubType = ReturnType<typeof subGetter>;
 
-	type Subscriber = (val: SubType) => Promise<void> | void;
-	const _subscriptions: Set<Subscriber> = new Set();
-	/**
-	 * Takes a function as an argument and adds it to the subscriber list.
-	 * @param subscriber - Subscriber
-	 */
-	function addSubscriber(subscriber: Subscriber) {
-		if (typeof subscriber !== 'function') {
-			throw new Error('Subscriber must be a function');
-		}
-		_subscriptions.add(subscriber);
-	}
-	/**
-	 * It deletes an event/subscriber from the subscriptions list.
-	 * @param subscriber - Subscriber
-	 */
-	function deleteSubscriber(subscriber: Subscriber) {
-		_subscriptions.delete(subscriber);
-	}
-	/**
-	 * It loops through the Set of subscribers and executes each function with the value passed in as
-	 * an argument
-	 * @param {SubType} val - The value that is being passed to the subscribers.
-	 */
-	function triggerSubscribers(val: SubType) {
-		_subscriptions.forEach(dep => dynamicallyExecuteFunction(dep, val));
-	}
-	type ValueMutator = (val: SubType) => SubType;
+	const _subscriptions: Set<Subscriber<SubType>> = new Set();
 	/**
 	 * It mutates the $value - for complex types that are typeof object.
 	 * @param mutator - (val: T) => T
 	 */
-	async function mutateSubscriber(mutator: ValueMutator) {
+	async function mutateSubscriber(mutator: ValueMutator<SubType>) {
 		if (typeof mutator !== 'function') {
 			throw new Error('Mutation must be a function');
 		}
 		subSetter(await dynamicallyExecuteFunction(mutator, subGetter()));
-		triggerSubscribers(subGetter());
+		triggerSubscribers(subGetter(), _subscriptions);
 	}
 
 	function setValue(val: SubType) {
 		// @ts-expect-error not an error maybe?
 		subSetter(val); 
-		triggerSubscribers(val);
+		triggerSubscribers(val, _subscriptions);
 	}
 
-	return {
+	const signal = {
 		get value(): SubType {
 			return subGetter();
 		},
@@ -73,42 +96,35 @@ export function useSubscription<T>(value: T, deep = false) {
 		get(): SubType {
 			return subGetter();
 		},
-		set(val: SubType | ValueMutator) {
+		set(val: SubType | ValueMutator<SubType>) {
 			// @ts-expect-error same here also ?
 			if (typeof val === 'function') setValue(val(subGetter()));
 			else setValue(val);
 		},
 
-		/**
-		 * A Subscriber(function) is executed when the value is changed.
-		 * @param subscriber - type Subscriber = (val: T) => Promise<void> | void;
-		 */
-		addSub(subscriber: Subscriber) {
-			addSubscriber(subscriber);
+		addSub(subscriber: Subscriber<SubType>) {
+			addSubscriber(subscriber, _subscriptions);
 		},
 		/**
 		 * A method that allows you to delete a subscriber from the Set of subscribers.
 		 * @param subscriber - Subscriber
 		 */
-		deleteSub(subscriber: Subscriber) {
-			deleteSubscriber(subscriber);
+		deleteSub(subscriber: Subscriber<SubType>) {
+			deleteSubscriber(subscriber, _subscriptions);
 		},
-		/**
-		 * Manually trigger subscribers. Shouldn't be used unless explicity needed (rarely).
-		 */
-		triggerSubs() {
-			triggerSubscribers(subGetter());
-		},
+		
+	} as P extends undefined | false ? Signal<SubType> : SignalWithMutate<SubType>;
 
-		/**
-		 * It mutates the value of the object.
-		 * @param mutator - (val: T) => T
-		 */
-		mutate(mutator: ValueMutator) {
+	if(!primitive) {
+		(signal as SignalWithMutate<SubType>).mutate = function (mutator: ValueMutator<SubType>): void {
 			if (typeof subGetter() !== 'object') {
 				throw new Error('Value passed is not an typeof object! Patch only accepts typeof object');
 			}
 			mutateSubscriber(mutator);
 		}
-	};
+
+	}
+
+	return signal;
 }
+
